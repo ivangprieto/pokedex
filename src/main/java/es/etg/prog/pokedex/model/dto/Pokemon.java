f2ee7e23e35ef97b895a91c86ca0114bfac7d86c
ivan <ivan@mail.com>
2026-04-08 16:10:14 +0200

    Agregado Lombok

--- src/main/java/es/etg/prog/pokedex/model/dto/Pokemon.java
+++ src/main/java/es/etg/prog/pokedex/model/dto/Pokemon.java
@@ -1,33 +1,13 @@
 package es.etg.prog.pokedex.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Pokemon {
     private final int numeroRegistro;
     private String nombre;
     private String tipo;
 
-    public Pokemon(int numeroRegistro, String nombre, String tipo) {
-        this.numeroRegistro = numeroRegistro;
-        this.nombre = nombre;
-        this.tipo = tipo;
-    }
-
-    public int getNumeroRegistro() {
-        return numeroRegistro;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
